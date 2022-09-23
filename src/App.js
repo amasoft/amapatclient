c44bcfe84ccde85cd4264ceba399921f920ebba7
amasoft <amadifaraday@gmail.com>
2022-09-23 03:23:26 -1000

    worked on the ui with boostrap

--- src/App.js
+++ src/App.js
@@ -20,26 +20,16 @@ import { useContext } from 'react';
 import { Context } from './context/Context';
 import Check from './pages/Check';
 function App() {
-  // const {user}=useContext(Context);
   return (
     <Router> 
-      {/* <TopBar/> */}
       <Check/>
      <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/post/:id" element={<Single/>}/>
     <Route path="/cat/:id" element={<Single/>}/>
-    //  <Route path="/write" element={<Write/>}/>
-
-      {/* <Route path="/login" element={user?<Home/>:<Login/>}/>
-    //  <Route path="/register" element={user ?<Home/>:<Register/>}/>
-    //  <Route path="/settings" element={user?<Settings/>:<Register/>}/>
-    //  <Route path="/write" element={user?<Write/>:<Register/>}/>
-    //  <Route path="/post/:id" element={<Single/>}/> */}
-
+      <Route path="/write" element={<Write/>}/>
      </Routes>
       </Router>
-    // <p>AMADI</p>
   );
 }
 
